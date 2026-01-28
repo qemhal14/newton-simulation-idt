d2da3eae99a205ac27de10afe56a02f97156d806
qemal <gembol1419@gmail.com>
2026-01-28 08:42:16 +0700

    update rsbuild.config.ts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,65 +1,16 @@
-import process from 'node:process';
-import path from 'node:path';
-import { defineConfig, rspack } from '@rsbuild/core';
+import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginSvgr } from '@rsbuild/plugin-svgr';
-import { pluginEslint } from "@rsbuild/plugin-eslint";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const SRC_PATH = path.resolve(process.cwd(), 'src');
-
-export default defineConfig(({ env }) => ({
+export default defineConfig({
   output: {
     distPath: {
-      root: 'build',
-    },
-  },
-  server: {
-    port: 8000,
-  },
-  dev: {
-    progressBar: false,
-  },
-  resolve: {
-    alias: {
-      '@elice/extcontent-apis': path.join(SRC_PATH, 'elice', 'apis'),
-      '@elice/extcontent-utils': path.join(SRC_PATH, 'elice', 'utils'),
-    },
-  },
-  tools: {
-    rspack: {
-      plugins: [
-        new rspack.DefinePlugin({
-          'process.env.REACT_APP_ELICE_EXTERNAL_CONTENT_API': JSON.stringify(
-            process.env.REACT_APP_ELICE_EXTERNAL_CONTENT_API
-          ),
-        }),
-      ],
+      root: 'dist', // Standard Netlify publish directory
     },
   },
   plugins: [
     pluginReact(),
-    pluginSvgr({
-      mixedImport: true,
-      svgrOptions: {
-        svgo: false,
-        exportType: 'named',
-      },
-    }),
-    pluginEslint({
-      enable: false,
-      eslintPluginOptions: {
-        cwd: __dirname,
-        fix: env !== 'production',
-        configType: 'eslintrc',
-      }
-    }),
   ],
   html: {
     template: './public/index.html',
   },
-}));
+});
